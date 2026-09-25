3d80e6907f044fe9d6b8aa38acc5db09a940394c
Bock, Benjamin <benjamin.bock@techem.de>
2026-09-25 21:44:55 +0200

    [R] Added kubelogin-linux-arm64.zip to get-sha256.sh script

--- get-sha256.sh
+++ get-sha256.sh
@@ -4,7 +4,7 @@ set -e
 
 [[ -z ${KUBELOGIN_VERSION} ]] && echo environment variable 'KUBELOGIN_VERSION' is required && exit 1
 
-ARCHIVES=(kubelogin-darwin-amd64.zip kubelogin-darwin-arm64.zip kubelogin-linux-amd64.zip)
+ARCHIVES=(kubelogin-darwin-amd64.zip kubelogin-darwin-arm64.zip kubelogin-linux-amd64.zip kubelogin-linux-arm64.zip)
 
 for zip in ${ARCHIVES[@]}; do
   curl -sL https://github.com/Azure/kubelogin/releases/download/v${KUBELOGIN_VERSION}/${zip}.sha256
